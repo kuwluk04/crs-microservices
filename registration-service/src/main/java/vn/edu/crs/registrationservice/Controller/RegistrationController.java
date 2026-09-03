b58b994a094fd52859c9b73de6c2bc83cda4ac73
kuwluk04 <2311060842@hunre.edu.vn>
2026-09-03 09:10:21 +0700

    feat(registration-service): add GET /registrations/my endpoint

--- registration-service/src/main/java/vn/edu/crs/registrationservice/Controller/RegistrationController.java
+++ registration-service/src/main/java/vn/edu/crs/registrationservice/Controller/RegistrationController.java
@@ -1,24 +1,35 @@
 package vn.edu.crs.registrationservice.Controller;
-import vn.edu.crs.registrationservice.dto.RegistrationRequestDTO;
-import vn.edu.crs.registrationservice.entity.Registration;
-import vn.edu.crs.registrationservice.service.RegistrationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import vn.edu.crs.registrationservice.dto.RegistrationRequestDTO;
+import vn.edu.crs.registrationservice.entity.Registration;
+import vn.edu.crs.registrationservice.service.RegistrationService;
+import java.util.List;
+
 @RestController
 @RequestMapping("/registrations")
 @RequiredArgsConstructor
 public class RegistrationController {
+
     private final RegistrationService registrationService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Registration register(@Valid @RequestBody
-                                 RegistrationRequestDTO dto) {
+    public Registration register(@Valid @RequestBody RegistrationRequestDTO dto) {
         return registrationService.register(dto);
     }
+
     @DeleteMapping("/{id}")
     public void cancel(@PathVariable Long id) {
         registrationService.cancel(id);
+    }
+
+    @GetMapping("/my")
+    public List<Registration> getMyRegistrations(Authentication authentication) {
+        Long studentId = (Long) authentication.getCredentials();
+        return registrationService.getMyRegistrations(studentId);
     }
 }
--- registration-service/src/main/java/vn/edu/crs/registrationservice/security/JwtAuthFilter.java
+++ registration-service/src/main/java/vn/edu/crs/registrationservice/security/JwtAuthFilter.java
@@ -39,12 +39,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                         .build()
                         .parseSignedClaims(token)
                         .getPayload();
-
                 String username = claims.getSubject();
                 String role = claims.get("role", String.class);
+                Long userId = claims.get("userId", Long.class);
                 var authToken = new UsernamePasswordAuthenticationToken(
-                        username, null, List.of(new
-                        SimpleGrantedAuthority("ROLE_" + role))
+                        username, userId, List.of(new SimpleGrantedAuthority("ROLE_" + role))
                 );
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             } catch (Exception e) {
